0cea4bcb908a076c1444b0122a9b25c3afbb6fb5
Joseph DiGiovanni <jdigiovanni78@gmail.com>
2024-09-29 09:35:07 -0400

    Add gaming device udev rules

--- nixos/configs/gaming.nix
+++ nixos/configs/gaming.nix
@@ -1,5 +1,12 @@
-{ custom-modules, ... }:
+{ custom-modules, pkgs, ... }:
 
 {
   imports = with custom-modules.nixos.software; [ steam ];
+
+  services.udev = {
+    enable = true;
+    packages = with pkgs; [
+      game-devices-udev-rules
+    ];
+  };
 }
