b55cf26d11a56dc7ce5320b49942e8d10d3042c0
Joseph DiGiovanni <jdigiovanni78@gmail.com>
2024-10-10 20:57:47 -0400

    Add steam controller support

--- nixos/configs/gaming.nix
+++ nixos/configs/gaming.nix
@@ -3,8 +3,25 @@
 {
   imports = with custom-modules.nixos.software; [ steam ];
 
+  boot.kernelModules = [ "hid_steam" ];
+
   services.udev = {
     enable = true;
+
+    extraRules = ''
+      # Valve USB devices
+      SUBSYSTEM=="usb", ATTRS{idVendor}=="28de", MODE="0666", TAG+="uaccess"
+
+      # Steam Controller udev write access
+      KERNEL=="uinput", SUBSYSTEM=="misc", TAG+="uaccess", OPTIONS+="static_node=uinput"
+
+      # Valve HID devices over USB hidraw
+      KERNEL=="hidraw*", ATTRS{idVendor}=="28de", MODE="0666", TAG+="uaccess"
+
+      # Valve HID devices over bluetooth hidraw
+      KERNEL=="hidraw*", KERNELS=="*28DE:*", MODE="0666", TAG+="uaccess"
+    '';
+
     packages = with pkgs; [
       game-devices-udev-rules
     ];
