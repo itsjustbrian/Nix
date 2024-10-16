fc31671064b30b7641ec3210b65d1eb8e9c5f374
Joseph DiGiovanni <jdigiovanni78@gmail.com>
2024-10-16 09:09:31 -0400

    Add OpenCL support for AMD GPUs

--- nixos/hardware/amd-graphics.nix
+++ nixos/hardware/amd-graphics.nix
@@ -1,3 +1,13 @@
+{ pkgs, ... }:
+
 {
-  hardware.amdgpu.initrd.enable = true;
+  hardware.amdgpu = {
+    initrd.enable = true;
+    opencl.enable = true;
+  };
+
+  # Workaround for software that uses hard-coded HIP libraries
+  systemd.tmpfiles.rules = [
+    "L+    /opt/rocm/hip   -    -    -     -    ${pkgs.rocmPackages.clr}"
+  ];
 }
