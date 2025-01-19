30f3a83e6a85e922ac1d119b1e4d5e5df3a430f6
Joseph DiGiovanni <jdigiovanni78@gmail.com>
2025-01-18 20:10:28 -0500

    Update Bluetooth Battery Meter extension options

--- home-manager/modules/gnome/extensions/bluetooth-battery-meter.nix
+++ home-manager/modules/gnome/extensions/bluetooth-battery-meter.nix
@@ -3,7 +3,11 @@
 {
   dconf.settings = {
     "org/gnome/shell".enabled-extensions = [ "Bluetooth-Battery-Meter@maniacx.github.com" ];
-    "org/gnome/shell/extensions/Bluetooth-Battery-Meter".enable-battery-level-text = true;
+    "org/gnome/shell/extensions/Bluetooth-Battery-Meter" = {
+      enable-battery-level-text = true;
+      level-indicator-color = 0; # 0: white, 1: colored, 2: custom
+      level-indicator-type = 1; # 0: bar, 1: dots
+    };
   };
 
   home.packages = with pkgs; [ gnomeExtensions.bluetooth-battery-meter ];
