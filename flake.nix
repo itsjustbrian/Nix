c8d795ac3160e7fdfe84190b914608c2dc42da65
Joseph DiGiovanni <jdigiovanni78@gmail.com>
2025-01-18 20:08:39 -0500

    Update flake

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
 
     firefox-gnome-theme = {
       flake = false;
-      url = "github:rafaelmardojai/firefox-gnome-theme/v133.1";
+      url = "github:rafaelmardojai/firefox-gnome-theme/v134";
     };
 
     haumea = {
