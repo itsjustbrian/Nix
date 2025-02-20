387893def3a4731ed2d285d89dbc6304df48a508
Joseph DiGiovanni <jdigiovanni78@gmail.com>
2025-02-20 11:11:15 -0500

    Change Joe-Desktop hardware configuration

--- nixos/hosts/Joe-Desktop/default.nix
+++ nixos/hosts/Joe-Desktop/default.nix
@@ -5,20 +5,15 @@
     ./file-systems.nix
     ./gdm/monitors
     ./home-manager
-    configs."3d-printing"
     configs.desktop
     configs.desktop-extras
     configs.gaming
     configs.monitor-control
     configs.secureboot # Must be commented on first install
-    configs.vr
     hardware.amd-graphics
-    hardware.msi-mpg-b650i-edge-wifi
+    hardware.msi-pro-b650p-wifi
     users.bferch
   ];
 
-  services = {
-    displayManager.autoLogin.user = "bferch";
-    fprintd.enable = true;
-  };
+  services.displayManager.autoLogin.user = "bferch";
 }
