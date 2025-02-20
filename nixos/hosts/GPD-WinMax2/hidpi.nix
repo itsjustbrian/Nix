a5d74bfcf5a854c84da0b42cb1f6674d978c55ae
Joseph DiGiovanni <jdigiovanni78@gmail.com>
2025-02-20 10:25:54 -0500

    Remove unnecessary hosts

--- nixos/hosts/GPD-WinMax2/hidpi.nix
+++
@@ -1,9 +0,0 @@
-{ ... }:
-
-{
-  home-manager.sharedModules = [
-    {
-      dconf.settings."org/gnome/shell/extensions/lennart-k/rounded_corners".corner-radius = 7;
-    }
-  ];
-}
--- nixos/hosts/Livingroom-PC/home-manager/gnome/extensions.nix
+++
@@ -1,9 +0,0 @@
-{ custom-modules, ... }:
-
-{
-  imports = with custom-modules.gnome.extensions; [
-    caffeine
-    hide-top-bar
-    no-titlebar-when-maximized
-  ];
-}
