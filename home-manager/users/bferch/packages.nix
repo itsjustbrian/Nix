b326d0aaa93c4b2905b987f622a7e5f24d71be4c
Joseph DiGiovanni <jdigiovanni78@gmail.com>
2025-02-20 11:07:51 -0500

    Customize user settings

--- home-manager/users/bferch/packages.nix
+++ home-manager/users/bferch/packages.nix
@@ -2,23 +2,10 @@
 
 {
   home.packages = with pkgs; [
-    blender
-    bottles
-    darktable
     discord
-    element-desktop
-    f3d
-    foliate
     gimp
-    github-desktop
-    handbrake
     localsend
     obs-studio
     onlyoffice-bin
-    openmw
-    openspades
-    transmission_4-gtk
-    youtube-music
-    via
   ];
 }
