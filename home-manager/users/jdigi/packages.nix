ade75c526b679abd45572f6a7592ceaee86ad114
Joseph DiGiovanni <jdigiovanni78@gmail.com>
2025-03-01 19:20:40 -0500

    Remove unused programs

--- home-manager/users/jdigi/packages.nix
+++ home-manager/users/jdigi/packages.nix
@@ -7,18 +7,15 @@
     darktable
     discord
     element-desktop
-    f3d
     foliate
     gimp
     github-desktop
-    handbrake
     localsend
     mission-center
     obs-studio
     onlyoffice-bin
     openmw
     openspades
-    transmission_4-gtk
     youtube-music
     via
   ];
