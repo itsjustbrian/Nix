60d5d2226529611d397bd4c9158fc5ab57cc28c0
Joseph DiGiovanni <jdigiovanni78@gmail.com>
2025-03-13 16:09:02 -0400

    VScode: Add Godot paths

--- home-manager/users/jdigi/vscode/extensions/godot-tools.nix
+++ home-manager/users/jdigi/vscode/extensions/godot-tools.nix
@@ -2,4 +2,9 @@
 
 {
   programs.vscode.extensions = with pkgs.vscode-marketplace; [ geequlim.godot-tools ];
+
+  programs.vscode.userSettings = {
+    "godotTools.editorPath.godot3" = "${pkgs.godot3}/bin/godot3";
+    "godotTools.editorPath.godot4" = "${pkgs.godot_4}/bin/godot4";
+  };
 }
